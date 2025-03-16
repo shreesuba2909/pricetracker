b33534c3a3c6fba85160c2e7ba67c85df71c7dbf
subashree <suba.jxk@gmail.com>
2025-03-16 13:52:52 +0530

    Initial commit

---
+++ app/page.tsx
@@ -0,0 +1,47 @@
+import HeroCarousel from "@/components/HeroCarousel";
+import Searchbar from "@/components/Searchbar";
+import Image from "next/image";
+import { getAllProducts } from "@/lib/actions";
+import ProductsCard from "@/components/ProductsCard";
+export const revalidate = 10;
+export default async function Home() {
+  const allProduct = await getAllProducts();
+ 
+  return (
+    <>
+      <section className="px-6  md:px-20 py-24">
+        <div className="flex max-xl:flex-col gap-6">
+          <div className="flex flex-col justify-center">
+            <p className="small-text">
+              Smart Shopping Starts:
+              <Image
+                src="/assets/icons/arrow-right.svg"
+                alt="arrow-right"
+                width={16}
+                height={16}
+              />
+            </p>
+            <h1 className="head-text">
+              Unleash the Power of
+              <span className="text-primary">PriceWise</span>
+            </h1>
+            <p className="mt-6">
+              Powerful, self-serve product and growth analytics to help you
+              convert, engage, and retain more.
+            </p>
+            <Searchbar />
+          </div>
+          <HeroCarousel />
+        </div>
+      </section>
+      <section className="trending-section">
+        <h2 className="section-text">Trending</h2>
+        <div className="flex flex-wrap gap-x-8 gap-y-16">
+          {allProduct?.map((product) => (
+           <ProductsCard key={product.id} product={product}/>
+          ))}
+        </div>
+      </section>
+    </>
+  );
+}
---
+++ lib/scraper/index.ts
@@ -0,0 +1,83 @@
+import axios from "axios";
+import * as cheerio from "cheerio";
+import {
+  extractCurrency,
+  extractDescription,
+  extractPrice,
+  extractScoreIcon,
+} from "../utils";
+export const maxDuration = 300; // This function can run for a maximum of 300 seconds
+export const dynamic = "force-dynamic";
+export const revalidate = 0;
+export async function scrapeAmazonProduct(url: string) {
+  if (!url) return;
+  //BrightData proxy config
+  const username = String(process.env.BRIGHT_DATA_USERNAME);
+  const password = String(process.env.BRIGHT_DATA_PASSWORD);
+  const port = 22225;
+  const session_id = (1000000 * Math.random()) | 0;
+  const options = {
+    auth: {
+      username: `${username}-session-${session_id}`,
+      password,
+    },
+    host: "brd.superproxy.io",
+    port,
+    rejectUnauthorized: false,
+  };
+  try {
+    //Fetch the product page
+    const response = await axios.get(url, options);
+    const $ = cheerio.load(response.data);
+    //Extract the product title
+    const title = $("#productTitle").text().trim();
+    const currentPrice = extractPrice(
+      $(".priceToPay span.a-price-whole"),
+      $(".a.size.base.a-color-price"),
+      $(".a-button-selected .a-color-base"),
+      $(".a-price.a-text-price")
+    );
+    const originalPrice = extractPrice(
+      $("#priceblock_ourprice"),
+      $(".a-price.a-text-price span.a-offscreen"),
+      $("#listPrice"),
+      $("#priceblock_dealprice"),
+      $(".a-size-base.a-color-price")
+    );
+    const ratings = $("#acrCustomerReviewText").text().trim();
+    const outOfStock = $(".a-color-success").text().trim() == "" ? false : true;
+    const images: string | void =
+      $("#imgBlkFront").attr("data-a-dynamic-image") ||
+      $("#landingImage").attr("data-a-dynamic-image") ||
+      "{}";
+    const imagesUrl = Object.keys(JSON.parse(images));
+    const currency = extractCurrency($(".a-price-symbol"));
+    const discountRate = $(".savingsPercentage").text().replace(/[-%]/g, "");
+    const starIcon = extractScoreIcon(
+      $(".a-size-base .a-color-base").text().trim()
+    );
+
+    const description = extractDescription($);
+    // Construct data object with scraped information
+    const data = {
+      url,
+      title,
+      currency: currency || "$",
+      image: imagesUrl[0],
+      currentPrice: Number(currentPrice) || Number(originalPrice),
+      originalPrice: Number(originalPrice) || Number(currentPrice),
+      priceHistory: [],
+      discountRate: Number(discountRate),
+      starIcon: starIcon || "",
+      description: description,
+      isOutOfStock: outOfStock,
+      productReviews: ratings || "",
+      lowestPrice: Number(currentPrice) || Number(originalPrice),
+      highestPrice: Number(originalPrice) || Number(currentPrice),
+      averagePrice: Number(currentPrice) || Number(originalPrice),
+    };
+    return data;
+  } catch (error: any) {
+    throw new Error(`Failed to scrape product:${error.message}`);
+  }
+}
